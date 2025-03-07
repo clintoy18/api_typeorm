56e38d542adda40b6e362553b2d40b3612baffe6
clintoy18 <aicsalonzo@gmail.com>
2025-03-08 00:37:22 +0800

    file conversion js to ts

---
+++ server.ts
@@ -0,0 +1,23 @@
+import "rootpath"; 
+import express from "express";
+import cors from "cors";
+import { errorHandler } from "./_middleware/error-handler";
+import userRoutes from "./users/user.controller";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
+app.use("/users", userRoutes);
+
+// Global error handler
+app.use(errorHandler);
+
+// Set up server port
+const PORT = process.env.NODE_ENV === "production" ? Number(process.env.PORT) || 80 : 4000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
